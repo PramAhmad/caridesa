f0ef46c05a705116a0fd275f613b66fefa4cb0e7
PramAhmad <pramuditaadit2810@gmail.com>
2025-06-14 03:09:57 +0700

    feat: add comprehensive profile, settings, backup, and activity log routes for tenant management

--- routes/tenant.php
+++ routes/tenant.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Tenant\ActivityLogController;
+use App\Http\Controllers\Tenant\BackupController;
+use App\Http\Controllers\Tenant\SettingsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
@@ -10,6 +13,7 @@ use Wave\Http\Controllers\HomeController;
 use App\Http\Controllers\Tenant\UsersController;
 use App\Http\Controllers\Tenant\RolesController;
 use App\Http\Controllers\Tenant\PermissionsController;
+use App\Http\Controllers\Tenant\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,13 +32,56 @@ Route::middleware([
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-        Route::get('/', [HomeController::class, 'home'])->name('home');
-        Route::middleware('auth')->group(function() {
-            Route::resource('users', UsersController::class);
-            Route::resource('roles', RolesController::class);
-            Route::resource('permissions', PermissionsController::class);
-        });
-      
+    Route::get('/', [HomeController::class, 'home'])->name('home');
+    
+    Route::middleware('auth')->group(function() {
+        Route::resource('users', UsersController::class);
+        Route::resource('roles', RolesController::class);
+        Route::resource('permissions', PermissionsController::class);
+    });
+     // Profile routes
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('profile', 'show')->name('profile.show');
+        Route::get('profile/edit', 'edit')->name('profile.edit');
+        Route::put('profile', 'update')->name('profile.update');
+        Route::get('profile/password', 'editPassword')->name('profile.password.edit');
+        Route::put('profile/password', 'updatePassword')->name('profile.password.update');
+    });
+
+    // Settings routes
+    Route::get('/settings/general', [SettingsController::class, 'general']);
+    Route::put('/settings/general', [SettingsController::class, 'updateGeneral']);
+    Route::get('/settings/security', [SettingsController::class, 'security']);
+    Route::put('/settings/security', [SettingsController::class, 'updateSecurity']);
+
+
+
+    Route::prefix('/settings/backups')->name('tenant.settings.')->middleware(['auth'])->group(function () {
+        Route::get('/', [BackupController::class, 'index'])->name('backups');
+        Route::post('/create', [BackupController::class, 'create'])->name('backups.create');
+        Route::get('/download/{filename}', [BackupController::class, 'download'])->name('backups.download');
+        Route::delete('/delete/{filename}', [BackupController::class, 'destroy'])->name('backups.destroy');
+        Route::post('/restore/{filename}', [BackupController::class, 'restore'])->name('backups.restore');
+    });
+    Route::prefix('/logs')->name('tenant.logs.')->middleware(['auth'])->group(function () {
+        Route::get('/', [ActivityLogController::class, 'index'])->name('index');
+        Route::get('/export', [ActivityLogController::class, 'export'])->name('export');
+        Route::delete('/clear', [ActivityLogController::class, 'clear'])->name('clear');
+        Route::get('/{id}', [ActivityLogController::class, 'show'])->name('show');
+    });
+
+    // Theme Management Routes
+    Route::prefix('/admin/themes')->name('tenant.themes.')->group(function () {
+        Route::get('/', [App\Http\Controllers\Tenant\ThemeController::class, 'index'])->name('index');
+        Route::get('/create', [App\Http\Controllers\Tenant\ThemeController::class, 'create'])->name('create');
+        Route::post('/', [App\Http\Controllers\Tenant\ThemeController::class, 'store'])->name('store');
+        Route::get('/{theme}/edit', [App\Http\Controllers\Tenant\ThemeController::class, 'edit'])->name('edit');
+        Route::put('/{theme}', [App\Http\Controllers\Tenant\ThemeController::class, 'update'])->name('update');
+        Route::delete('/{theme}', [App\Http\Controllers\Tenant\ThemeController::class, 'destroy'])->name('destroy');
+        Route::patch('/{theme}/activate', [App\Http\Controllers\Tenant\ThemeController::class, 'activate'])->name('activate');
+        Route::get('/{theme}/edit-content', [App\Http\Controllers\Tenant\ThemeController::class, 'editContent'])->name('edit-content');
+        Route::put('/{theme}/update-content', [App\Http\Controllers\Tenant\ThemeController::class, 'updateContent'])->name('update-content');
+    });
     
     Route::get('/user', function () {
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id') . Auth::user();
