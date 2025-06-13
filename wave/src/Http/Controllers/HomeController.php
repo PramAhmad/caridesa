83bee29c937f345328d783937c4d2b44e9bee59c
PramAhmad <pramuditaadit2810@gmail.com>
2025-06-13 21:56:04 +0700

    feat: create home view and update HomeController to return it

--- wave/src/Http/Controllers/HomeController.php
+++ wave/src/Http/Controllers/HomeController.php
@@ -16,25 +16,10 @@ class HomeController extends Controller
 
     public function home()
     {
-        return view('tenant.home');
+        dd(Auth::user());
     }
     public function index()
     {
-    	if(setting('auth.dashboard_redirect', true) != "null"){
-    		if(!Auth::guest()){
-    			return redirect('dashboard');
-    		}
-    	}
-
-        $seo = [
-
-            'title'         => setting('site.title', 'Laravel Wave'),
-            'description'   => setting('site.description', 'Software as a Service Starter Kit'),
-            'image'         => url('/og_image.png'),
-            'type'          => 'website'
-
-        ];
-
-        return view('theme::home', compact('seo'));
+        return view('central.home');
     }
 }
